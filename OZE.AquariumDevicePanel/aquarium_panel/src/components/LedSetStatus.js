41ccdc0a18d771784a6136322138007f59935955
dawid krzus <dkrzus92@gmail.com>
2018-07-25 23:28:15 +0200

    Change way to display led status

--- OZE.AquariumDevicePanel/aquarium_panel/src/components/LedSetStatus.js
+++ OZE.AquariumDevicePanel/aquarium_panel/src/components/LedSetStatus.js
@@ -2,10 +2,16 @@ import React from 'react';
 import {Badge} from 'reactstrap';
 
 export const LetSetStatus = (props) => {
-    let status = <Badge color="danger">Off</Badge>;
+    let status;
 
-    if(props.ledStatus){
+    if(props.ledStatus === "on"){
         status = <Badge color="success">On</Badge>;
+    }
+    else if(props.ledStatus === "off"){
+        status = <Badge color="success">On</Badge>;
+    }
+    else {
+        status = <Badge color="primary">Off</Badge>;
     }
 
     return <div>
