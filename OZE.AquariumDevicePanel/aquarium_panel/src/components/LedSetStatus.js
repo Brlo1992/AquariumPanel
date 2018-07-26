de353b7fd286bcc8a8682622dfd6bd1d1f299ee5
dawid krzus <dkrzus92@gmail.com>
2018-07-26 22:41:19 +0200

    Fix led status

--- OZE.AquariumDevicePanel/aquarium_panel/src/components/LedSetStatus.js
+++ OZE.AquariumDevicePanel/aquarium_panel/src/components/LedSetStatus.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import {Badge} from 'reactstrap';
+import { Badge } from 'reactstrap';
 
 export const LetSetStatus = (props) => {
     let status;
 
-    if(props.ledStatus === "on"){
+    if (props.ledStatus === "on") {
         status = <Badge color="success">On</Badge>;
     }
-    else if(props.ledStatus === "off"){
-        status = <Badge color="success">On</Badge>;
+    else if (props.ledStatus === "off") {
+        status = <Badge color="danger">Off</Badge>;
     }
     else {
-        status = <Badge color="primary">Off</Badge>;
+        status = <Badge color="primary"> - </Badge>;
     }
 
     return <div>
