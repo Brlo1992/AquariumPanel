a3642f17a582c06a808ee0f248cecb2fb41db395
dawid krzus <dkrzus92@gmail.com>
2018-07-27 22:46:31 +0200

    Add component to show scheduled tasks

---
+++ OZE.AquariumDevicePanel/aquarium_panel/src/components/ScheduledTasks.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default class ScheduledTasks extends React.Component {
+    render() {
+        return <div>
+            {props.children}
+        </div>
+    }
+}
