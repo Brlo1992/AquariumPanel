5baa7a17f6542641e0966f68db468e6c6021be20
dawid krzus <dkrzus92@gmail.com>
2018-07-28 08:15:37 +0200

    Move task record to separate component

---
+++ OZE.AquariumDevicePanel/aquarium_panel/src/components/ScheduledTaskRow.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import {Row, Col, Button} from 'reactstrap'
+import * as Fa from "react-icons/lib/fa";
+
+export const ScheduledTaskRow = (props) => {
+    return <Row></Row>
+}
+
+export default ScheduledTaskRow
