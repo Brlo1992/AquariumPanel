ab53e09f67a7bd828daea49bc1f856e50706fa14
dawid krzus <dkrzus92@gmail.com>
2018-07-28 08:13:44 +0200

    Set valid font size for table

--- OZE.AquariumDevicePanel/aquarium_panel/src/components/ScheduledTasks.js
+++ OZE.AquariumDevicePanel/aquarium_panel/src/components/ScheduledTasks.js
@@ -10,28 +10,28 @@ export default class ScheduledTasks extends React.Component {
                 <Row><Col><h2>Scheduled Tasks</h2></Col></Row>
                 <br />
                 <Row>
-                    <Col><h5>Action name</h5></Col>
-                    <Col><h5>Status</h5></Col>
-                    <Col><h5>Execution time</h5></Col>
-                    <Col><h5>Last execution</h5></Col>
-                    <Col md="3"><h5>Possible actions</h5></Col>
+                    <Col><h4>Action name</h4></Col>
+                    <Col><h4>Status</h4></Col>
+                    <Col><h4>Execution time</h4></Col>
+                    <Col><h4>Last execution</h4></Col>
+                    <Col md="3"><h4>Possible actions</h4></Col>
                 </Row>
                 <hr />
                 <Row>
-                    <Col>Turn on all leds</Col>
-                    <Col>On</Col>
-                    <Col>09:15:01</Col>
-                    <Col>12/12/2017 09:15:01</Col>
+                    <Col><p>Turn on all leds</p></Col>
+                    <Col><p>On</p></Col>
+                    <Col><p>09:15:01</p></Col>
+                    <Col><p>12/12/2017 09:15:01</p></Col>
                     <Col md="1"><Button  color="success" block><Fa.FaEdit/></Button></Col>
                     <Col md="1"><Button  color="primary" block><Fa.FaBolt/></Button></Col>
                     <Col md="1"><Button  color="danger" block><Fa.FaTrash/></Button></Col>
                 </Row>
                 <br />
                 <Row>
-                    <Col>Turn on all leds</Col>
-                    <Col>On</Col>
-                    <Col>09:15:01</Col>
-                    <Col>12/12/2017 09:15:01</Col>
+                    <Col><p>Turn on all leds</p></Col>
+                    <Col><p>On</p></Col>
+                    <Col><p>09:15:01</p></Col>
+                    <Col><p>12/12/2017 09:15:01</p></Col>
                     <Col md="1"><Button  color="success" block><Fa.FaEdit/></Button></Col>
                     <Col md="1"><Button  color="primary" block><Fa.FaBolt/></Button></Col>
                     <Col md="1"><Button  color="danger" block><Fa.FaTrash/></Button></Col>
