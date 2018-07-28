42d0ea091aa9c4c39d7fe9c5a483c32756fb071e
dawid krzus <dkrzus92@gmail.com>
2018-07-28 07:56:49 +0200

    Try to find good design

--- OZE.AquariumDevicePanel/aquarium_panel/src/components/ScheduledTasks.js
+++ OZE.AquariumDevicePanel/aquarium_panel/src/components/ScheduledTasks.js
@@ -15,12 +15,23 @@ export default class ScheduledTasks extends React.Component {
                     <Col><h5>Last execution</h5></Col>
                     <Col><h5>Possible actions</h5></Col>
                 </Row>
+                <hr />
                 <Row>
                     <Col>Turn on all leds</Col>
                     <Col>On</Col>
                     <Col>09:15:01</Col>
                     <Col>12/12/2017 09:15:01</Col>
-                    <Col></Col>
+                    <Col><Button color="danger" block >Remove</Button></Col>
+                    <Col><Button color="primary" block >Run</Button></Col>
+                </Row>
+                <br />
+                <Row>
+                    <Col>Turn on all leds</Col>
+                    <Col>On</Col>
+                    <Col>09:15:01</Col>
+                    <Col>12/12/2017 09:15:01</Col>
+                    <Col><Button color="danger" block>Remove</Button></Col>
+                    <Col><Button color="primary" block>Run</Button></Col>
                 </Row>
             </Col>
         </Row>
