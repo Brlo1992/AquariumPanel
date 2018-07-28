7bf8d038ff03e7a86b869f6c8e0e597404f4af37
dawid krzus <dkrzus92@gmail.com>
2018-07-28 08:43:02 +0200

    Bind actions to buttons

--- OZE.AquariumDevicePanel/aquarium_panel/src/components/ScheduledTaskRow.js
+++ OZE.AquariumDevicePanel/aquarium_panel/src/components/ScheduledTaskRow.js
@@ -3,15 +3,28 @@ import { Row, Col, Button } from 'reactstrap'
 import * as Fa from "react-icons/lib/fa";
 
 export default class ScheduledTaskRow extends React.Component {
+
+    edit = () => {
+        console.log("edit");
+    }
+
+    remove = () => {
+        console.log("remove");
+    }
+
+    run = () => {
+        console.log("run");
+    }
+
     render() {
         return <Row>
             <Col><p>{this.props.name}</p></Col>
             <Col><p>{this.props.status}</p></Col>
             <Col><p>{this.props.timeExecution}</p></Col>
             <Col><p>{this.props.lastTimeExecution}</p></Col>
-            <Col md="1"><Button color="success" block><Fa.FaEdit /></Button></Col>
-            <Col md="1"><Button color="primary" block><Fa.FaBolt /></Button></Col>
-            <Col md="1"><Button color="danger" block><Fa.FaTrash /></Button></Col>
+            <Col md="1"><Button onClick={() => this.edit()} color="success" block><Fa.FaEdit /></Button></Col>
+            <Col md="1"><Button onClick={() => this.run()} color="primary" block><Fa.FaBolt /></Button></Col>
+            <Col md="1"><Button onClick={() => this.remove()} color="danger" block><Fa.FaTrash /></Button></Col>
         </Row>
     }
 }
