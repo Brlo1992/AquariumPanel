a40de5a50b2eb53530e3de42709fc52f5afcc9c9
dawid krzus <dkrzus92@gmail.com>
2018-07-28 17:22:40 +0200

    Works with task jobs

--- OZE.AquariumDevicePanel/aquarium_panel/src/components/ScheduledTasks.js
+++ OZE.AquariumDevicePanel/aquarium_panel/src/components/ScheduledTasks.js
@@ -1,9 +1,16 @@
 import React from 'react'
 import { Row, Col, Button } from 'reactstrap';
-import * as Fa from "react-icons/lib/fa";
+import ScheduledTaskRow from './ScheduledTaskRow'
 
 
 export default class ScheduledTasks extends React.Component {
+    constructor(props){
+        super(props);
+
+        this.state ={
+            taskJobs : []
+        };
+    }
     render() {
         return <Row>
             <Col>
@@ -17,25 +24,10 @@ export default class ScheduledTasks extends React.Component {
                     <Col md="3"><h4>Possible actions</h4></Col>
                 </Row>
                 <hr />
-                <Row>
-                    <Col><p>Turn on all leds</p></Col>
-                    <Col><p>On</p></Col>
-                    <Col><p>09:15:01</p></Col>
-                    <Col><p>12/12/2017 09:15:01</p></Col>
-                    <Col md="1"><Button  color="success" block><Fa.FaEdit/></Button></Col>
-                    <Col md="1"><Button  color="primary" block><Fa.FaBolt/></Button></Col>
-                    <Col md="1"><Button  color="danger" block><Fa.FaTrash/></Button></Col>
-                </Row>
                 <br />
-                <Row>
-                    <Col><p>Turn on all leds</p></Col>
-                    <Col><p>On</p></Col>
-                    <Col><p>09:15:01</p></Col>
-                    <Col><p>12/12/2017 09:15:01</p></Col>
-                    <Col md="1"><Button  color="success" block><Fa.FaEdit/></Button></Col>
-                    <Col md="1"><Button  color="primary" block><Fa.FaBolt/></Button></Col>
-                    <Col md="1"><Button  color="danger" block><Fa.FaTrash/></Button></Col>
-                </Row>
+                    <ScheduledTaskRow />
+                    <ScheduledTaskRow />
+                    <ScheduledTaskRow />
                 <br />
             </Col>
         </Row>
