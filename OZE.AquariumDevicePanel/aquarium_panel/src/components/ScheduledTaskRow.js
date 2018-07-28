be81e697e7e63544c532ad952d3511b44fc6c872
dawid krzus <dkrzus92@gmail.com>
2018-07-28 08:40:25 +0200

    Change component type

--- OZE.AquariumDevicePanel/aquarium_panel/src/components/ScheduledTaskRow.js
+++ OZE.AquariumDevicePanel/aquarium_panel/src/components/ScheduledTaskRow.js
@@ -1,9 +1,17 @@
 import React from 'react'
-import {Row, Col, Button} from 'reactstrap'
+import { Row, Col, Button } from 'reactstrap'
 import * as Fa from "react-icons/lib/fa";
 
-export const ScheduledTaskRow = (props) => {
-    return <Row></Row>
+export default class ScheduledTaskRow extends React.Component {
+    render() {
+        return <Row>
+            <Col><p>{this.props.name}</p></Col>
+            <Col><p>{this.props.status}</p></Col>
+            <Col><p>{this.props.timeExecution}</p></Col>
+            <Col><p>{this.props.lastTimeExecution}</p></Col>
+            <Col md="1"><Button color="success" block><Fa.FaEdit /></Button></Col>
+            <Col md="1"><Button color="primary" block><Fa.FaBolt /></Button></Col>
+            <Col md="1"><Button color="danger" block><Fa.FaTrash /></Button></Col>
+        </Row>
+    }
 }
-
-export default ScheduledTaskRow
