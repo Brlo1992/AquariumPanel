0aed74bd53fb95fac85d94b8f965e488853ea7e6
dawid krzus <dkrzus92@gmail.com>
2018-08-01 23:21:08 +0200

    Fill modal

--- OZE.AquariumDevicePanel/aquarium_panel/src/components/ScheduledTaskModal.js
+++ OZE.AquariumDevicePanel/aquarium_panel/src/components/ScheduledTaskModal.js
@@ -1,21 +1,38 @@
 import React from 'react';
-import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter, Form, FormGroup, Label, Input,  } from 'reactstrap';
 
 export default class ScheduledTaskModal extends React.Component {
-  render() {
-    return (
-      <div>
-        <Modal isOpen={this.props.modal} toggle={this.props.toggle}>
-          <ModalHeader toggle={this.toggle}>New Scheduled Task</ModalHeader>
-          <ModalBody>
-              I will add new scheduled tasks
-          </ModalBody>
-          <ModalFooter>
-            <Button color="success" onClick={this.addNewScheduledTask}>Add</Button>{' '}
-            <Button color="danger" onClick={this.props.toggle}>Cancel</Button>
-          </ModalFooter>
-        </Modal>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <Modal isOpen={this.props.modal} toggle={this.props.toggle}>
+                    <ModalHeader toggle={this.toggle}>New Scheduled Task</ModalHeader>
+                    <ModalBody>
+                        <Form>
+                            <FormGroup>
+                                <Label>Name</Label>
+                                <Input type="name" name="name" id="name" placeholder="Scheduled task name" />
+                            </FormGroup>
+                            <FormGroup>
+                                <Label>Url action</Label>
+                                <Input type="urlAction" name="urlAction" id="urlAction" placeholder="Scheduled task url action" />
+                            </FormGroup>
+                            <FormGroup>
+                                <Label>Status</Label>
+                                <Input type="status" name="status" id="status" placeholder="Scheduled task status" />
+                            </FormGroup>
+                            <FormGroup>
+                                <Label>Execution time</Label>
+                                <Input type="executionTime" name="executionTime" id="executionTime" placeholder="Scheduled task execution time" />
+                            </FormGroup>
+                        </Form>
+                    </ModalBody>
+                    <ModalFooter>
+                        <Button color="success" onClick={this.addNewScheduledTask}>Add</Button>{' '}
+                        <Button color="danger" onClick={this.props.toggle}>Cancel</Button>
+                    </ModalFooter>
+                </Modal>
+            </div>
+        );
+    }
 }
