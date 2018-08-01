4914e880f3c7bdc869c901f036c173401cdcbffa
dawid krzus <dkrzus92@gmail.com>
2018-08-01 23:27:46 +0200

    Change type for inputs

--- OZE.AquariumDevicePanel/aquarium_panel/src/components/ScheduledTaskModal.js
+++ OZE.AquariumDevicePanel/aquarium_panel/src/components/ScheduledTaskModal.js
@@ -11,24 +11,27 @@ export default class ScheduledTaskModal extends React.Component {
                         <Form>
                             <FormGroup>
                                 <Label>Name</Label>
-                                <Input type="name" name="name" id="name" placeholder="Scheduled task name" />
+                                <Input type="text" name="name" id="name" placeholder="Scheduled task name" />
                             </FormGroup>
                             <FormGroup>
                                 <Label>Url action</Label>
-                                <Input type="urlAction" name="urlAction" id="urlAction" placeholder="Scheduled task url action" />
+                                <Input type="url" name="urlAction" id="urlAction" placeholder="Scheduled task url action" />
                             </FormGroup>
                             <FormGroup>
                                 <Label>Status</Label>
-                                <Input type="status" name="status" id="status" placeholder="Scheduled task status" />
+                                <Input type="select" name="status" id="status" placeholder="Scheduled task status">
+                                    <option>ON</option>
+                                    <option>OFF</option>
+                                </Input>
                             </FormGroup>
                             <FormGroup>
                                 <Label>Execution time</Label>
-                                <Input type="executionTime" name="executionTime" id="executionTime" placeholder="Scheduled task execution time" />
+                                <Input type="time" name="executionTime" id="executionTime" placeholder="Scheduled task execution time" />
                             </FormGroup>
                         </Form>
                     </ModalBody>
                     <ModalFooter>
-                        <Button color="success" onClick={this.addNewScheduledTask}>Add</Button>{' '}
+                        <Button color="success" onClick={this.addNewScheduledTask}>Add</Button>
                         <Button color="danger" onClick={this.props.toggle}>Cancel</Button>
                     </ModalFooter>
                 </Modal>
