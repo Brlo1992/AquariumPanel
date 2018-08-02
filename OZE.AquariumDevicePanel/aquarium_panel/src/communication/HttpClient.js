71359c05ee8df83e401e53812b0eb3356bb664b4
dawid krzus <dkrzus92@gmail.com>
2018-08-02 21:53:09 +0200

    Add post method to httpclient

--- OZE.AquariumDevicePanel/aquarium_panel/src/communication/HttpClient.js
+++ OZE.AquariumDevicePanel/aquarium_panel/src/communication/HttpClient.js
@@ -4,34 +4,44 @@ class HttpClient {
     getFullUrl = (url) => this.baseUrl.concat(url);
 
     handleResponse = (response) => {
-        if(response.status === 200){
+        if (response.status === 200) {
             return response.json();
         }
         return Promise.reject(response);
     }
 
     get = (url, func) => {
-        return fetch(this.getFullUrl(url))  
+        return fetch(this.getFullUrl(url))
             .then(response => response.json())
-            .then(data => { 
+            .then(data => {
                 console.log(data);
                 func(data);
             })
             .catch(error => console.log(error));
-   }
+    }
 
     getAsync = async (url) => {
         let result = await fetch(this.getFullUrl(url))
-                                .then(this.handleResponse)
-                                .then(data => {
-                                    console.log(data);
-                                    return data;
-                                })
-                                .catch(error => console.log(error));
-        
+            .then(this.handleResponse)
+            .then(data => {
+                console.log(data);
+                return data;
+            })
+            .catch(error => console.log(error));
+
         console.log(result);
 
         return result;
+    }
+
+    post = (url, data, func) =>{
+        return fetch(this.getFullUrl(url))
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            func(data);
+        })
+        .catch(error => console.log(error));
     }
 }
 export default new HttpClient();
