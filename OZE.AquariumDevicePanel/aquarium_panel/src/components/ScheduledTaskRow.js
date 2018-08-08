7488c3fdcc450e2e7688d0298ede64db88c7616d
dawid krzus <dkrzus92@gmail.com>
2018-08-08 23:23:11 +0200

    Works with remove/edit actions

--- OZE.AquariumDevicePanel/aquarium_panel/src/components/ScheduledTaskRow.js
+++ OZE.AquariumDevicePanel/aquarium_panel/src/components/ScheduledTaskRow.js
@@ -1,19 +1,16 @@
 import React from 'react'
 import { Row, Col, Button } from 'reactstrap'
 import * as Fa from "react-icons/lib/fa";
+import HttpClient from "../communication/HttpClient";
 
 export default class ScheduledTaskRow extends React.Component {
 
-    edit = () => {
-        console.log("edit");
-    }
-
     remove = () => {
-        console.log("remove");
+        HttpClient.get
     }
 
     run = () => {
-        console.log("run");
+        
     }
 
     render() {
@@ -23,7 +20,7 @@ export default class ScheduledTaskRow extends React.Component {
             <Col><p>{this.props.taskJob.status}</p></Col>
             <Col><p>{this.props.taskJob.timeExecution}</p></Col>
             <Col><p>{this.props.taskJob.lastTimeExecution}</p></Col>
-            <Col md="1"><Button onClick={() => this.edit()} color="success" block><Fa.FaEdit /></Button></Col>
+            <Col md="1"><Button onClick={this.props.edit} color="success" block><Fa.FaEdit /></Button></Col>
             <Col md="1"><Button onClick={() => this.run()} color="primary" block><Fa.FaBolt /></Button></Col>
             <Col md="1"><Button onClick={() => this.remove()} color="danger" block><Fa.FaTrash /></Button></Col>
         </Row>
